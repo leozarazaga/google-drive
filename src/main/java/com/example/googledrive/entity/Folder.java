64390024a7c0c9fd45c06191b55c97144930f20d
Leo Zarazaga <lza1313@outlook.com>
2023-11-27 17:55:45 +0100

    Added Folder and User entities, implemented database functionality

--- src/main/java/com/example/googledrive/entity/Folder.java
+++ src/main/java/com/example/googledrive/entity/Folder.java
@@ -1,2 +1,25 @@
-package com.example.googledrive.entity;public class Folder {
+package com.example.googledrive.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+@Table(name = "folder")
+public class Folder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String folderName;
 }
--- src/main/java/com/example/googledrive/entity/User.java
+++ src/main/java/com/example/googledrive/entity/User.java
@@ -1,2 +1,26 @@
-package com.example.googledrive.entity;public class User {
+package com.example.googledrive.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    private String username;
+
+    private String email;
+
 }
